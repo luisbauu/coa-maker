352a5b6e29a257f0750d78b84f4859a5563b1c23
Jose Luis Bautista <josebauluis.48@gmail.com>
2023-05-09 04:19:49 +0800

    LGTM 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,24 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>DISCS COA MAKER</title>
+        <meta
+          name="description"
+          content="Create Certificates of Authenticity for DISCS projects with ease."
+        />
+        <meta
+          name="keywords"
+          content="certificate of authenticity, COA, DISCS, ADMU, Ateneo, Ateneo de Manila, Ateneo de Manila University,"
+        />
+        <meta name="author" content="Jose Luis Bautista" />
+        <meta
+          property="og:title"
+          content="Create Certificates of Authenticity for DISCS Projects with ease."
+        />
+        <meta
+          property="og:description"
+          content="Create Certificates of Authenticity for DISCS projects with ease."
+        />
+        <meta property="og:type" content="website" />
       </Head>
       <Component {...pageProps} />
     </>
