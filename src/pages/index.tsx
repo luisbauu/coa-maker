c2c4099b0608d5c45b0767985a31d8d7d6924937
Jose Luis Bautista <jose.luis.bautista@obf.ateneo.edu>
2023-05-10 17:47:05 +0800

    added autocomplete and made it better

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,115 +2,23 @@ import { useState } from "react";
 import { ChangeEvent } from "react";
 import download from "downloadjs";
 import toast, { Toaster } from "react-hot-toast";
-import CourseSearch from "@/components/CourseSearch";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
-import courses from "../../public/course.json";
+import CoursesJSON from "../../public/course.json";
+import CourseInstructorJSON from "../../public/courseInstructors.json";
 
 type Item = {
   id: number;
   code: string;
-  courseTitle?: string;
-  courseInstructor?: string;
+  courseTitle: string;
+  courseInstructor: string;
+};
+
+type Item2 = {
+  id: number;
+  name: string;
 };
 
 export default function Home() {
-  // const courses: Item[] = [
-  //   { id: 0, name: "CS 197" },
-  //   { id: 1, name: "CSCI 21" },
-  //   { id: 2, name: "CSCI 22" },
-  //   { id: 3, name: "CSCI 40" },
-  //   { id: 4, name: "CSCI 42" },
-  //   { id: 5, name: "CSCI 51.01" },
-  //   { id: 6, name: "CSCI 51.02" },
-  //   { id: 7, name: "CSCI 61" },
-  //   { id: 8, name: "CSCI 71" },
-  //   { id: 9, name: "CSCI 111" },
-  //   { id: 10, name: "CSCI 112" },
-  //   { id: 11, name: "CSCI 113i" },
-  //   { id: 12, name: "CSCI 115" },
-  //   { id: 13, name: "CSCI 117i" },
-  //   { id: 14, name: "CSCI 130" },
-  //   { id: 15, name: "CSCI 133" },
-  //   { id: 16, name: "CSCI 134.1i" },
-  //   { id: 17, name: "CSCI 140" },
-  //   { id: 18, name: "CSCI 142i" },
-  //   { id: 19, name: "CSCI 173" },
-  //   { id: 20, name: "CSCI 181.03" },
-  //   { id: 21, name: "CSCI 182.06" },
-  //   { id: 22, name: "CSCI 184.03" },
-  //   { id: 23, name: "CSCI 197" },
-  //   { id: 24, name: "CSCI 199.1" },
-  //   { id: 25, name: "CSCI 199.2" },
-  //   { id: 26, name: "CSCI 199.3" },
-  //   { id: 27, name: "CSCI 203" },
-  //   { id: 28, name: "CSCI 204" },
-  //   { id: 29, name: "CSCI 207" },
-  //   { id: 30, name: "CSCI 209" },
-  //   { id: 31, name: "CSCI 211" },
-  //   { id: 32, name: "CSCI 215" },
-  //   { id: 33, name: "CSCI 217" },
-  //   { id: 34, name: "CSCI 234.1" },
-  //   { id: 35, name: "CSCI 240" },
-  //   { id: 36, name: "CSCI 241" },
-  //   { id: 37, name: "CSCI 242" },
-  //   { id: 38, name: "CSCI 271" },
-  //   { id: 39, name: "CSCI 273" },
-  //   { id: 40, name: "CSCI 282.06" },
-  //   { id: 41, name: "CSCI 296" },
-  //   { id: 42, name: "CSCI 298.1" },
-  //   { id: 43, name: "CSCI 298.4" },
-  //   { id: 44, name: "CSCI 298.6" },
-  //   { id: 45, name: "CSCI 299.1" },
-  //   { id: 46, name: "CSCI 299.2" },
-  //   { id: 47, name: "CSCI 299.4" },
-  //   { id: 48, name: "CSCI 299.5" },
-  //   { id: 49, name: "CSCI 299.6" },
-  //   { id: 50, name: "CSCI 299.7" },
-  //   { id: 51, name: "CSCI 314" },
-  //   { id: 52, name: "CSCI 315" },
-  //   { id: 53, name: "CSCI 347" },
-  //   { id: 54, name: "CSCI 371" },
-  //   { id: 55, name: "CSCI 390.1" },
-  //   { id: 56, name: "CSCI 390.2" },
-  //   { id: 57, name: "CSCI 390.4" },
-  //   { id: 58, name: "CSCI 396" },
-  //   { id: 59, name: "CSCI 399.1" },
-  //   { id: 60, name: "CSCI 399.2" },
-  //   { id: 61, name: "CSCI 399.4" },
-  //   { id: 62, name: "CSCI 399.5" },
-  //   { id: 63, name: "CSCI 399.6" },
-  //   { id: 64, name: "CSCI 399.7" },
-  //   { id: 65, name: "GDEV 21i" },
-  //   { id: 66, name: "GDEV 30" },
-  //   { id: 67, name: "GDEV 32" },
-  //   { id: 68, name: "GDEV 42" },
-  //   { id: 69, name: "GDEV 198.3" },
-  //   { id: 70, name: "ISCS 30.14" },
-  //   { id: 71, name: "ISCS 30.16" },
-  //   { id: 72, name: "ISCS 30.18" },
-  //   { id: 73, name: "ISCS 30.23" },
-  //   { id: 74, name: "ISCS 30.28" },
-  //   { id: 75, name: "ISCS 30.36" },
-  //   { id: 76, name: "ISCS 30.37" },
-  //   { id: 77, name: "ISCS 30.45" },
-  //   { id: 78, name: "ISCS 30.53" },
-  //   { id: 79, name: "ISCS 30.66" },
-  //   { id: 80, name: "ISCS 30.67" },
-  //   { id: 81, name: "MSYS 22" },
-  //   { id: 82, name: "MSYS 40" },
-  //   { id: 83, name: "MSYS 42" },
-  //   { id: 84, name: "MSYS 51" },
-  //   { id: 85, name: "MSYS 112" },
-  //   { id: 86, name: "MSYS 116" },
-  //   { id: 87, name: "MSYS 121i" },
-  //   { id: 88, name: "MSYS 142" },
-  //   { id: 89, name: "MSYS 197" },
-  //   { id: 90, name: "MSYS 198.1" },
-  //   { id: 91, name: "MSYS 198.3" },
-  //   { id: 92, name: "MSYS 199.3" },
-  //   { id: 93, name: "MSYS 171" },
-  // ];
-
   const [members, setMembers] = useState([{ name: "" }]);
   const [title, setTitle] = useState<string>("");
   const [type, setType] = useState<string>("");
@@ -119,6 +27,7 @@ export default function Home() {
     new Date().toISOString().substring(0, 10)
   );
   const [sources, setSources] = useState<string>("");
+  const [section, setSection] = useState<string>("");
   const [courseCodeSection, setCourseCodeSection] = useState<string>("");
   const [courseTitle, setCourseTitle] = useState<string>("");
   const [courseInstructor, setCourseInstructor] = useState<string>("");
@@ -172,8 +81,26 @@ export default function Home() {
     setMembers(data);
   };
 
-  const handleAutocompleteSelect = (item: string) => {
-    setCourseCodeSection(item);
+  const handleAutocompleteSelect = (name: string, item: string) => {
+    const inputMap: Record<
+      string,
+      React.Dispatch<React.SetStateAction<string>>
+    > = {
+      courseCodeSection: setCourseCodeSection,
+      courseTitle: setCourseTitle,
+      courseInstructor: setCourseInstructor,
+    };
+
+    if (name == "courseCodeSection") {
+    }
+
+    inputMap[name](item);
+  };
+
+  const handleSectionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSection(value);
+    setCourseCodeSection((prev) => prev + value);
   };
 
   const generatePdf = async (e: any) => {
@@ -186,7 +113,17 @@ export default function Home() {
     }
 
     if (!type) {
-      toast.error("Please select a type of submission");
+      toast.error("Please select a type of submission.");
+      return;
+    }
+
+    if (type == "other" && specifiedType == "") {
+      toast.error("Please input a specified type of submission!");
+      return;
+    }
+
+    if (!section) {
+      toast.error("Please input a section for your course code.");
       return;
     }
 
@@ -353,32 +290,52 @@ export default function Home() {
         <div className="flex flex-row gap-5 p-5 self-start w-full">
           <p className="text-2xl font-bold ">Course Code & Section</p>
 
-          <div className="w-full">
+          <div className="w-5/6">
             <ReactSearchAutocomplete<Item>
-              items={courses}
+              items={CoursesJSON}
               fuseOptions={{ keys: ["code"] }} // Search on both fields
               resultStringKeyName="code" // String to display in the results
-              onSelect={(item) => handleAutocompleteSelect(item.code)}
+              onSelect={(item) => {
+                handleAutocompleteSelect("courseCodeSection", item.code);
+                handleAutocompleteSelect("courseTitle", item.courseTitle);
+              }}
               showIcon={false}
               styling={{
                 borderRadius: "0.5rem",
                 boxShadow: "none",
                 height: "4rem",
+                zIndex: 3,
               }}
             />
           </div>
-
-          {/* <input
+          <input
             type="text"
-            name="courseCodeSection"
-            value={courseCodeSection}
-            onChange={handleChange}
-            className="border w-full rounded-lg p-2"
-          /> */}
+            name="section"
+            value={section}
+            onChange={handleSectionChange}
+            className="border w-1/6 rounded-lg p-2"
+          />
         </div>
 
         <div className="flex flex-row gap-5 p-5 self-start w-full">
           <p className="text-2xl font-bold  ">Course Title</p>
+          {/*<div className="w-full">
+           <ReactSearchAutocomplete<Item>
+              items={CoursesJSON}
+              fuseOptions={{ keys: ["courseTitle"] }} // Search on both fields
+              resultStringKeyName="courseTitle" // String to display in the results
+              onSelect={(item: Item) =>
+                handleAutocompleteSelect("courseTitle", item.courseTitle)
+              }
+              showIcon={false}
+              styling={{
+                borderRadius: "0.5rem",
+                boxShadow: "none",
+                height: "4rem",
+                zIndex: 2,
+              }}
+            />
+          </div> */}
           <input
             type="text"
             name="courseTitle"
@@ -390,13 +347,20 @@ export default function Home() {
 
         <div className="flex flex-row gap-5 p-5 self-start w-full">
           <p className="text-2xl font-bold ">Course Instructor</p>
-          <input
-            type="text"
-            name="courseInstructor"
-            value={courseInstructor}
-            onChange={handleChange}
-            className="border w-full rounded-lg p-2"
-          />
+          <div className="w-full">
+            <ReactSearchAutocomplete<Item2>
+              items={CourseInstructorJSON}
+              onSelect={(item: Item2) =>
+                handleAutocompleteSelect("courseInstructor", item.name)
+              }
+              showIcon={false}
+              styling={{
+                borderRadius: "0.5rem",
+                boxShadow: "none",
+                height: "4rem",
+              }}
+            />
+          </div>
         </div>
         {docType ? (
           <>
@@ -439,7 +403,6 @@ export default function Home() {
             <input
               className="border w-full rounded-lg p-3 "
               name="name"
-              placeholder="Name"
               onChange={(event) => handleFormChange(event, 0)}
               value={members[0].name}
             />
